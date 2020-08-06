c4e922c39d59bc36fe5a83f11fb04320a682ee92
Christian.Katsch <Christian.Katsch@devolo.de>
2020-08-06 15:38:34 +0200

    Replaces static mocks by create_mocks.sh
    You can define a profile with --profile=<NAME>. The profile, if not
    being build in, will be represented by a directory. You can copy
    the directory to a new one, change the content of the files to the
    desired ones and use the new directory as a new profile.
    A not existing profile will be created as directory and filled
    with default values.
    You can use as may profiles you like, they are applied in the
    specified order and will overwrite settings.
    
      Build in profiles:
         default -> Speedport Smart 3
                        * DEVICE_SpeedportSmart3
                        * TR181
                        * DATASET_1
    
         DEVICE_SpeedportSmart3 \
         DEVICE_Fritz7590        }-> mocking this device
         DEVICE_Fritz7490       /
         TR181   -> responding to GetParameterValues for
                    Device.DSL.Line.1.TestParams. with 200 and data
         TR098   -> responding to X_GENERIC_GetVDSLInfo with
                    HTTP 200 and data
         200 -> will respond with HTTP 200 and data
         500 -> will respond with HTTP 500 error 401 Invalid Action
         DATASET_1 -> one data set from speedport
         DATASET_2 -> another data set from fritzbox
    
    The mock can then be found using SSDP and will serve the data from
    the selected profile directory.
    Also the IP address to be announced can be set by --IP=<IP>,
    default is 127.0.0.1

--- home0/inserts/bin/099_home0_start_tr064.sh
+++ home0/inserts/bin/099_home0_start_tr064.sh
@@ -3,16 +3,11 @@
 # make mockserver run on IPv4
 sed -i 's/listen(port/listen(port, "0.0.0.0"/g' /usr/local/lib/node_modules/mockserver/bin/mockserver.js
 
-# start TR-064 moc server
-python3 /opt/mockserver_handler.py --log=/opt/mock.log
-
-# replace home with local IP address to announce where TR-064 will be reachable
-sed -i "s/127.0.0.1/${LOCAL_IP_ADDRESS}/g" /opt/ssdp_mock.py
-
 # Accept connections to the TR-064 mock server
 iptables -I INPUT 1 -p tcp --dport 49000 -j ACCEPT
 
-# start to declare TR-064
-python3 /opt/ssdp_handler.py --log=/opt/ssdp.log
+# start TR-064 moc server
+cd /opt
+sudo ./create_mocks.sh --IP="${LOCAL_IP_ADDRESS}"
 
 mylogger "TR-064 set up!"
--- home0/inserts/tr064/ssdp-mocks/ssdp_mock.py
+++ home0/inserts/tr064/ssdp-mocks/ssdp_mock.py
@@ -1,158 +1,116 @@
 #!/usr/bin/env python3
 # A Python Script to create a mock SSDP server that answers to a SSDP discovery request
 
-import socket
-import sys
+import argparse
+import datetime
 import logging
 import signal
-import os
-import argparse
-from datetime import date
-from time import gmtime
-
-terminate = False
-logger = logging.getLogger(__name__)
-pid_filename: str = "ssdp-mock-port{}.pid"
+import socket
+import sys
 
 
-
-def inform_parent_failed():
-    """ Inform Parent that server has finished setting up and is now ready"""
-    parent_pid = os.getppid()
-    if parent_pid == 1:
-        return True
-    os.kill(parent_pid, signal.SIGUSR1)
-    return False
-
-
-class MockSSDPServer():
-    def __init__(self, ip="0.0.0.0", port=1900, tr64Ip="127.0.0.1",
-                 tr64Port=49000, timeout=5):
-        self.ip = ip
-        self.port = port
-        self.cachecontrol = 1800
-        self.tr64Ip = tr64Ip
-        self.tr64Port = tr64Port
-        self.servernames = ["Fritzbox 747", "Speedport 42"]
-        self.urns = ["urn:dslforum-org:service:WANDSLInterfaceConfig:1#X_AVM-DE_GetDSLInfo",
-                     "urn:telekom-de:device:TO_InternetGatewayDevice:2#GetParameterValues"]
-        self.count = 0
-
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.setsockopt(
-            socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)  # ttl 2
-        self.sock.settimeout(timeout)
-
-        addr = socket.inet_aton('239.255.255.250')  # multicast address
-        interface = socket.inet_aton(self.ip)
-        self.sock.setsockopt(
-            socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, addr + interface)
-
-        self.sock.bind((self.ip, self.port))
-
-    def check(self, data, addr):
-        logger.info('SSDP Packet received from {}'.format(addr))
-        logger.info(data)
-        decoded = data.decode("utf8").replace('\r', '').split('\n')
-        msgtype, decoded = decoded[0], decoded[1:]
-        decodeddict = {x.split(':',  1)[0].upper(): x.split(
-            ':', 1)[1].strip(' ') for x in decoded if x != ''}
-
-        if 'M-SEARCH' in msgtype:
-            st = decodeddict.get('ST')
-
-            response = ['HTTP/1.1 200 OK',
-                        'CACHE-CONTROL: max-age={}'.format(self.cachecontrol),
-                        'LOCATION: http://{}:{}/description.xml'.format(
-                            self.tr64Ip, self.tr64Port),
-                        'SERVER: {}'.format(self.servernames[self.count % 2]),
-                        'Date: {}'.format(date.today().strftime(
-                            "%a, %d %b %Y %H:%M:%S")),
-                        'EXT: ',
-                        'ST: {}'.format(self.urns[self.count % 2]),
-                        'USN: {}'.format("uuid:00000000-0000-0002-0000-44fffffffe3b38ffffffa2ffffffb4::urn:telekom-de:device")]
-
-            if st == "urn:dslforum-org:device:InternetGatewayDevice:1":
-                # Only increase count on AVM SSDP receive, since we always send out both requests
-                self.count += 1
-
-            response.extend(('', ''))
-            response = '\r\n'.join(response)
-
-            self.sock.sendto(response.encode(), addr)
-            logger.info('SSDP Packet sent to {}'.format(addr))
-
-    def start(self):
-        logger.info('SSDP Server Running...')
-        countTimeout = pcount = 0
-        if inform_parent_failed():
-            logger.error(
-                'Parent already dead before successful startup finished.')
-            return
-        while not terminate:
-            try:
-                if countTimeout % 5 == 0:
-                    logger.info('Ssdp Poll... {} pings'.format(pcount))
-                    pcount = 0
-                    countTimeout = 1
-                data, addr = self.sock.recvfrom(1024)
-                pcount += 1
-                self.check(data, addr)
-            except socket.timeout:
-                countTimeout += 1
-                continue
-            except Exception as e:
-                logger.info('error occurred ' + str(e))
-
-
-def service_shutdown(signum, frame):
-    global terminate
-    terminate = True
-
-
-def pidfile_exists(port: int):
-    return os.path.isfile(pid_filename.format(port))
-
-
-def main():
-    parser = argparse.ArgumentParser(description='define Log location')
-    parser.add_argument('--logfile', action='store')
-    parser.add_argument('--port', action='store', type=int, default=1900,
-                        choices=range(1000, 65535))
-    args = parser.parse_args()
+def setup_logger(logfile: str):
     logger.setLevel(logging.DEBUG)
-    if args.logfile:
-        ch = logging.FileHandler(filename=args.logfile)
+    if logfile:
+        ch = logging.FileHandler(filename=logfile)
     else:
         ch = logging.StreamHandler(sys.stdout)
     ch.setLevel(logging.INFO)
     logger.addHandler(ch)
-    if pidfile_exists(args.port):
-        logger.error(
-            "Error: PID file found - process already running - shutting down...")
-        sys.exit(1)
-    else:
-        signal.signal(signal.SIGTERM, service_shutdown)
-        signal.signal(signal.SIGINT, service_shutdown)
-        try:
-            with open(pid_filename.format(args.port), 'w') as f:
-                f.write(str(os.getpid()))
-            server = MockSSDPServer()
-            server.start()
-        except OSError:
-            logger.error('Writing PID file failed.')
-        except Exception as e:
-            logger.error('Error: {}'.format(str(e)))
 
-    server.sock.close()
-    logger.info("Shutting down Ssdp server")
-    if pidfile_exists(args.port):
-        logger.info("Removing PID file Ssdp server")
-        os.remove(pid_filename.format(args.port))
-    ch.flush()
-    ch.close()
+
+class MockSSDPServer:
+    def __init__(self, ip="0.0.0.0", port=1900, location_ip="127.0.0.1", location_port=49000, timeout=5, server_name="Speedport Smart 3",
+                 st="urn:telekom-de:device:TO_InternetGatewayDevice:2"):
+        self.ip = ip
+        self.port = port
+        self.location_ip = location_ip
+        self.location_port = location_port
+        self.timeout = timeout
+        self.server_name = server_name
+        self.st = st
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # close socket on sigint and sigterm
+        signal.signal(signal.SIGINT, self.stop)
+        signal.signal(signal.SIGTERM, self.stop)
+
+    @staticmethod
+    def _check(data: bytes):
+        logger.info(data)
+        decoded = data.decode("utf8").replace('\r', '').split('\n')
+        msgtype, decoded = decoded[0], decoded[1:]
+        return 'M-SEARCH' in msgtype
+
+    def _send_mock_response(self, addr: str):
+        response = ['HTTP/1.1 200 OK',
+                    'CACHE-CONTROL: max-age=1800',
+                    'LOCATION: http://{}:{}/description.xml'.format(
+                        self.location_ip, self.location_port),
+                    'SERVER: {}'.format(self.server_name),
+                    'Date: {}'.format(datetime.date.today().strftime(
+                        "%a, %d %b %Y %H:%M:%S")),
+                    'EXT: ',
+                    'ST: {}'.format(self.st),
+                    'USN: {}{}'.format("uuid:00000000-0000-0002-0000-44fffffffe3b38ffffffa2ffffffb4::",self.st)]
+
+        response.extend(('', ''))
+        response = '\r\n'.join(response)
+        self.sock.sendto(response.encode(), addr)
+        logger.info('SSDP Packet sent to {}'.format(addr))
+
+    def start(self):
+        try:
+            addr = socket.inet_aton('239.255.255.250')  # multicast address
+            interface = socket.inet_aton(self.ip)
+            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)  # ttl 2
+            self.sock.settimeout(self.timeout)
+            self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, addr + interface)
+            self.sock.bind((self.ip, self.port))
+        except socket.error as e:
+            logger.error(f"Could not open socket: {e}")
+            sys.exit(1)
+
+        logger.info('SSDP Server Running...')
+        while True:
+            try:
+                data, addr = self.sock.recvfrom(1024)
+                msearch_received = self._check(data=data)
+                if msearch_received:
+                    self._send_mock_response(addr=addr)
+            except socket.timeout:
+                logger.info('socket timeout encountered')
+
+    def stop(self, signum, frame):
+        while True:
+            try:
+                self.sock.close()
+                sys.exit(0)
+            except socket.error:
+                sys.exit(1)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Simple Mockserver for SSDP discovery')
+    parser.add_argument('--logfile', action='store', help="Write output to <logfile>")
+    parser.add_argument('--port', action='store', type=int, default=1900, choices=range(1000, 65535), help="Server port for SSDP discovery.")
+    parser.add_argument('--st', action='store', default="urn:telekom-de:device:TO_InternetGatewayDevice:2",
+                        help="Mock value for the ST parameter in the SSDP response.")
+    parser.add_argument('--server-name', dest="server_name", action='store', help="Mock value for the SSDP server name.")
+    parser.add_argument('--location-ip', dest="location_ip", action='store', help="Mock value for the IP address inside the LOCATION parameter.")
+    parser.add_argument('--location-port', dest="location_port", action='store', type=int, default=49000,
+                        help="Mock value for the Port inside the LOCATION parameter.")
+
+    args = parser.parse_args()
+    setup_logger(logfile=args.logfile)
+
+    try:
+        server = MockSSDPServer(location_ip=args.location_ip, location_port=args.location_port, server_name=args.server_name, st=args.st)
+        server.start()
+    except Exception as e:
+        logger.error('Error: {}'.format(str(e)))
 
 
 if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
     sys.exit(main())
