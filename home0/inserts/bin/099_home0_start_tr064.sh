5b8a0554d5e6225e07c0b81df97df20b72954a13
P-Fleer <60703860+P-Fleer@users.noreply.github.com>
2022-05-17 12:55:33 +0200

    Add additional ports for the mock server

--- home0/inserts/bin/099_home0_start_tr064.sh
+++ home0/inserts/bin/099_home0_start_tr064.sh
@@ -4,7 +4,10 @@
 sed -i 's/listen(port/listen(port, "0.0.0.0"/g' /usr/local/lib/node_modules/mockserver/bin/mockserver.js
 
 # Accept connections to the TR-064 mock server
+iptables -I INPUT 1 -p tcp --dport 5438  -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 37215 -j ACCEPT
 iptables -I INPUT 1 -p tcp --dport 49000 -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 49001 -j ACCEPT
 
 # start TR-064 moc server
 cd /opt
